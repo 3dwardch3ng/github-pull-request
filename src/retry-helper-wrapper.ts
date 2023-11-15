1c37bb403336e28c0dcbe2ea5be4127d66fe2a8d
Edward Cheng <edward@cheng.sydney>
2023-11-16 01:15:24 +1100

    Chore: fix test of retry-helper-wrapper.ts

--- src/retry-helper-wrapper.ts
+++ src/retry-helper-wrapper.ts
@@ -5,19 +5,14 @@ export const defaultMinSeconds: number = 10;
 export const defaultMaxSeconds: number = 20;
 
 export function createRetryHelperWithDefaults(): IRetryHelper {
-  return new RetryHelper(
-    defaultMaxAttempts,
-    defaultMinSeconds,
-    defaultMaxSeconds,
-    undefined
-  );
+  return createRetryHelper();
 }
 
 export function createRetryHelper(
-  maxAttempts: number | undefined,
-  minSeconds: number | undefined,
-  maxSeconds: number | undefined,
-  attemptsInterval: number | undefined
+  maxAttempts?: number,
+  minSeconds?: number,
+  maxSeconds?: number,
+  attemptsInterval?: number
 ): IRetryHelper {
   return new RetryHelper(
     maxAttempts === undefined ? defaultMaxAttempts : Math.floor(maxAttempts),
@@ -30,17 +25,12 @@ export function createRetryHelper(
 export async function executeWithDefaults<T>(
   action: (...vars: unknown[]) => Promise<T>
 ): Promise<T> {
-  const retryHelper: IRetryHelper = createRetryHelper(
-    defaultMaxAttempts,
-    defaultMinSeconds,
-    defaultMaxSeconds,
-    undefined
-  );
+  const retryHelper: IRetryHelper = createRetryHelperWithDefaults();
   return await retryHelper.execute(action);
 }
 
 export async function executeWithCustomised<T>(
-  maxAttempts: number,
+  maxAttempts: number | undefined,
   minSeconds: number | undefined,
   maxSeconds: number | undefined,
   attemptsInterval: number | undefined,
